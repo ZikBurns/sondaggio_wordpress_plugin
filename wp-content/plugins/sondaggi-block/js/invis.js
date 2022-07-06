eb8240392b98acae4d804c45ff602d4616fea3cf
pepecalero <pepecalero@gmail.com>
2022-07-06 12:02:35 +0200

    Vis invis deleted

--- wp-content/plugins/sondaggi-block/js/invis.js
+++
@@ -1,6 +0,0 @@
-debugger;
-
-const frontsondaggio = document.querySelector('#frontSondaggio');  
-if(frontsondaggio) frontsondaggio.style.visibility = 'hidden';
-const resultschart = document.querySelector('#resultChart');        
-if(resultschart) resultschart.style.visibility='visible';
--- wp-content/plugins/sondaggi-block/js/vis.js
+++
@@ -1,5 +0,0 @@
-debugger;
-const frontsondaggio = document.querySelector('#frontSondaggio');        
-if(frontsondaggio) frontsondaggio.style.visibility = 'visible';
-const resultschart = document.querySelector('#resultChart');        
-if(resultschart) resultschart.style.visibility='hidden';
