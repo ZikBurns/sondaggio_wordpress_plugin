61cd69f89bfec9e4edaa8dc5f8cc6c8af49a2972
pepecalero <pepecalero@gmail.com>
2022-07-07 12:50:24 +0200

    api rest finished

--- wp-content/plugins/sondaggi-block/includes/gutenpride/build/index.asset.php
+++ wp-content/plugins/sondaggi-block/includes/gutenpride/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '90801d681e982c9f6963');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '39515b4f3118e9fb5aed');
